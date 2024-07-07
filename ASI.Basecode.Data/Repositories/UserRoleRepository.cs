ac16e4ff3bb0378dfea3a920e1bdb620dbb64dfa
Dave Noceda <deibeunoceda26@gmail.com>
2024-07-08 03:31:29 +0800

    Add UserRole Repository

---
+++ ASI.Basecode.Data/Repositories/UserRoleRepository.cs
@@ -0,0 +1,25 @@
+﻿using ASI.Basecode.Data.Interfaces;
+using ASI.Basecode.Data.Models;
+using Basecode.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASI.Basecode.Data.Repositories
+{
+    public class UserRoleRepository : BaseRepository, IUserRoleRepository
+    {
+
+        public UserRoleRepository (IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+
+        }
+
+        public IEnumerable<UserRole> RetrieveAll()
+        {
+            return this.GetDbSet<UserRole>();
+        }
+    }
+}
