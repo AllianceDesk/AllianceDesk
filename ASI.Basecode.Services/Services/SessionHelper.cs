34b77a55687ab67f04b6b720cf61f8111b02de39
Dave Noceda <deibeunoceda26@gmail.com>
2024-07-11 10:16:08 +0800

    Add Logic for getting the UserId of the current session

---
+++ ASI.Basecode.Services/Services/SessionHelper.cs
@@ -0,0 +1,34 @@
+﻿using ASI.Basecode.Data.Interfaces;
+using ASI.Basecode.Data.Models;
+using ASI.Basecode.Data.Repositories;
+using ASI.Basecode.Services.Interfaces;
+using ASI.Basecode.Services.Manager;
+using ASI.Basecode.Services.ServiceModels;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using static ASI.Basecode.Resources.Constants.Enums;
+
+namespace ASI.Basecode.Services.Services
+{
+    public class SessionHelper : ISessionHelper
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public SessionHelper(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public Guid GetUserIdFromSession()
+        {
+            var session = _httpContextAccessor.HttpContext.Session;
+            var userIdString = session.GetString("UserId");
+            return Guid.TryParse(userIdString, out var userId) ? userId : Guid.Empty;
+        }
+    }
+}
