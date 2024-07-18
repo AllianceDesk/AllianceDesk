a02ae6b7f1a6660f757248ba3d55dd2203b5fb3c
Dave Noceda <deibeunoceda26@gmail.com>
2024-07-19 01:17:07 +0800

    Add Favorite Backend on Data

---
+++ ASI.Basecode.Data/Repositories/FavoriteRepository.cs
@@ -0,0 +1,37 @@
+﻿using ASI.Basecode.Data.Interfaces;
+using ASI.Basecode.Data.Models;
+using Basecode.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASI.Basecode.Data.Repositories
+{
+    public class FavoriteRepository : BaseRepository, IFavoriteRepository
+    {
+
+        public FavoriteRepository(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+
+        }
+
+        public IEnumerable<Favorite> RetrieveAll()
+        {
+            return this.GetDbSet<Favorite>();
+        }
+
+        public void Add(Favorite favorite)
+        {
+            this.GetDbSet<Favorite>().Add(favorite);
+            UnitOfWork.SaveChanges();
+        }
+
+        public void Delete(Favorite favorite)
+        {
+            this.GetDbSet<Favorite>().Remove(favorite);
+            UnitOfWork.SaveChanges();
+        }
+    }
+}
